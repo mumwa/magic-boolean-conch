18d1a9d329e3759ae65f2860611e7dd16f4fca46
박효정 <baghyojeong@baghyojeong-ui-MacBookAir.local>
2022-11-16 20:50:51 +0900

    Fix : add arguement for typescript version

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,16 @@
 export class Conch {
-    ask(): boolean | string | undefined {
-        const random = Math.random();
-        if (random < 0.2) {
-            return false;
-        } else if (random < 0.4) {
-            return 'false';
-        } else if (random < 0.6) {
-            return undefined;
-        } else if (random < 0.8) {
-            return 'true';
-        } else if (random < 1) {
-            return true;
-        }
+  ask(question: any): boolean | string | undefined {
+    const random = Math.random();
+    if (random < 0.2) {
+      return false;
+    } else if (random < 0.4) {
+      return "false";
+    } else if (random < 0.6) {
+      return undefined;
+    } else if (random < 0.8) {
+      return "true";
+    } else if (random < 1) {
+      return true;
     }
+  }
 }
